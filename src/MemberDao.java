51f687a7e33d673ea4deb8754a3c5693e181fead
KangJinWoo <cuteretti@naver.com>
2016-03-19 15:50:34 +0900

    no message

---
+++ src/MemberDao.java
@@ -0,0 +1,225 @@
+
+
+import java.sql.*;
+import java.util.*;
+
+import javax.sql.*;
+import javax.naming.*;
+
+public class MemberDao {
+	private static MemberDao instance; // = new MemberDao();
+
+	private MemberDao() {
+	}
+
+	public static MemberDao getInstance() {
+		if (instance == null)
+			instance = new MemberDao();
+		return instance;
+	}
+
+	public Connection getConnection() { // Singleton
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/OracleDB");
+			conn = ds.getConnection();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+
+	public int idChk(String id) throws SQLException {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select id from member2 where id = ?";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next())
+				result = 1;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return result;
+	}
+
+	public int insert(Member mem) throws SQLException {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "insert into member2 values(?,?,?,?,?,sysdate)";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mem.getId());
+			pstmt.setString(2, mem.getPasswd());
+			pstmt.setString(3, mem.getName());
+			pstmt.setString(4, mem.getAddress());
+			pstmt.setString(5, mem.getTel());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return result;
+	}
+
+	public int loginChk(String id, String passwd) throws SQLException {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "Select passwd from member2 where id=?";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				String dbPass = rs.getString(1); // ("password")
+				if (dbPass.equals(passwd))
+					result = 1; // 일치
+				else
+					result = 0; // 암호가 다름
+			} else
+				result = -1; // 데이터 읽기 실패
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return result;
+	}
+
+	public int delete(String id) throws SQLException {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "delete from member2 where id = ?";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return result;
+	}
+
+	public Member select(String id) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "Select * from member2 where id=?";
+		Member mem = new Member();
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				mem.setId(rs.getString("id"));
+				mem.setPasswd(rs.getString("passwd"));
+				mem.setName(rs.getString("name"));
+				mem.setAddress(rs.getString("address"));
+				mem.setTel(rs.getString("tel"));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return mem;
+	}
+
+	public int update(Member mem) throws SQLException {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "update member2 set passwd = ?, name=?, address =?, tel=? where id = ?";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mem.getPasswd());
+			pstmt.setString(2, mem.getName());
+			pstmt.setString(3, mem.getAddress());
+			pstmt.setString(4, mem.getTel());
+			pstmt.setString(5, mem.getId());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return result;
+	}
+
+	public List<Member> selectList() throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "Select * from member2";
+		List<Member> list = new ArrayList<>();
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Member mem = new Member();
+				mem.setId(rs.getString("id"));
+				mem.setName(rs.getString("name"));
+				mem.setAddress(rs.getString("address"));
+				mem.setTel(rs.getString("tel"));
+				mem.setReg_date(rs.getDate("reg_date"));
+				list.add(mem);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (conn != null)
+				conn.close();
+		}
+		return list;
+	}
+}

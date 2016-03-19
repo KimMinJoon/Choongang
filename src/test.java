fadbf53dab7ea19e3f9089e410aba9f5c6a41f01
KangJinWoo <cuteretti@naver.com>
2016-03-19 14:51:50 +0900

    no message

--- src/test.java
+++ src/test.java
@@ -2,15 +2,9 @@
 public class test {
 	public static void main(String[] args) {
 
-		System.out.println("사실 강진우씨는 천재임");
+		System.out.println("밥먹어요 제발");
 
-		System.out.println("점심안드세요?");
-		
-		System.out.println("밥 2시에 먹는다면서요 지금 40분 넘었어요");
-		
-		System.out.println("정말 귀찮게 하시네ㅎㅎ");
-		System.out.println("짜증도 나네 ㅎㅎ");
-
+	
 	}
 
 }

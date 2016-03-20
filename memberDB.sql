b91cfc422d91159fccda2fc993454d11d24e32ab
KimMinJoon <wow2314@naver.com>
2016-03-20 21:28:22 +0900

    memberDB sql 파일 생성 및 수정 (확정)
    멤버 디비 속성 결정했고
    이걸기반으로 J_member 클래스 생성

--- memberDB.sql
+++ memberDB.sql
@@ -1,11 +1,9 @@
-select * from member1;
-
 create table j_member (
-	m_no number primary key,
+	m_no number not null,
 	m_email varchar2(30) not null,
-	m_reg_date date not null default sysdate,
-	m_nick varchar2(20) not null unique,
-	m_passwd varchar2(20) not null ,
-	lang_code varchar2(10) not null,
-	m_del_yn char(1) not null default 'n'
-));
+	m_reg_date date default sysdate,
+	m_passwd varchar2(20) not null,
+	m_del_yn char(1) default 'n',
+	l_code varchar2(5),
+	c_code varchar2(5)
+);

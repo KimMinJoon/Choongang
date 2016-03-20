686edf5a686a99d56cc38c277c285980768f7785
KimMinJoon <wow2314@naver.com>
2016-03-20 21:25:13 +0900

    J_Member / J_MemberDao 클래스 생성
    ---------------------------------------
    
    **** J_Member
    
    필요한 겟셋 설정
    
    --------------------------------
    
    **** J_MemberDao
    
    1. dbclose 생성
    2. 커넥션 풀 생성
    3. 싱글톤 생성

---
+++ src/J_MemberDao.java
@@ -0,0 +1,73 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.mysql.jdbc.PreparedStatement;
+
+public class J_MemberDao {
+	// 싱글톤 객체 생성을 낭비하지 않기위해
+	private static J_MemberDao instance = new J_MemberDao();
+
+	private J_MemberDao() {
+	}
+
+	public static J_MemberDao getInstance() {
+		return instance;
+	}
+
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();// 연결
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/OracleDB");// 커넥션
+																					// 풀
+			conn = ds.getConnection();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}// getConnection
+
+	// conn과 pstmt,그리고 rs를 종료해주는 메서드
+	// 나중에 닫을 때 편하게 사용하면된다!
+	public void dbClose(PreparedStatement pstmt, Connection conn) {
+
+		try {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}// dbClose(pstmt,conn)
+
+	public void dbClose(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // dbClose(rs,pstmt,conn)
+	}
+
+	
+	
+	
+}

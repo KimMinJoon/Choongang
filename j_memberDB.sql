08bce31aa91ed7642fceff15f91c829c060c6d41
GwakJiEun <je928@daum.net>
2016-03-25 20:57:56 +0900

    회원가입시스템 수정 - 이메일 중복 체크 수정 - 닉네임 varchar2 수정 - 닉네임 maxlength 수정

--- j_memberDB.sql
+++ j_memberDB.sql
@@ -2,7 +2,7 @@ create table j_member (
 	m_no number primary key,
 	m_email varchar2(30) not null unique,
 	m_passwd varchar2(20) not null,
-  	m_nick varchar2(15) not null unique,
+  	m_nick varchar2(30) not null unique,
 	m_reg_date date default sysdate,
 	m_del_yn char(1) default 'n',
 	c_code varchar2(30),

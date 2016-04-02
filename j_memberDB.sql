f8e1112eb3a169adfa2b29976716ef2ebf8423aa
강진우 <cuteretti@naver.com>
2016-04-02 17:46:26 +0900

    member 테이블  속성 순서 정리

--- j_memberDB.sql
+++ j_memberDB.sql
@@ -4,15 +4,18 @@ create table j_member (
 	m_passwd varchar2(20) not null,
   	m_nick varchar2(30) not null unique,
 	m_reg_date date default sysdate,
-	m_del_yn char(1) default 'n',
 	m_out_date date,
+	m_del_yn char(1) default 'n',
 	c_code varchar2(30),
 	l_code varchar2(30)
 );
 
 select * from j_member;
 
+insert into j_member values (1,'master',1,'관리자',sysdate,null,'n','c_default','l_defult');
+-- 관리자 insert
+
+
 --drop table j_member;
 
-alter table j_member add (m_out_date date);
 

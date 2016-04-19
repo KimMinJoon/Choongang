b4f01c570907a3d6d27f98b8051312a6c322c95e
곽지은 <je928@daum.net>
2016-04-19 15:46:32 +0900

    추천게시판 마이바티스 수정중2

--- src/j_recommendboard/J_RecommendBoardDao.java
+++ src/j_recommendboard/J_RecommendBoardDao.java
@@ -10,6 +10,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import j_noticeboard.J_NoticeBoard;
+
 public class J_RecommendBoardDao {
 	
 	private static J_RecommendBoardDao instance = new J_RecommendBoardDao();
@@ -32,95 +34,32 @@ public class J_RecommendBoardDao {
 	
 	public int selectTotal(String searchType, String searchTxt) {
 		int total = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = "select count(*) from j_recommendboard jrb, j_member m where jrb.m_no=m.m_no and brd_del_yn='n'";
-		String sql2 = "";
-		if(searchType.equals("all")) {
-			sql2 = "and (brd_subject like '%" + searchTxt + "%' or brd_content like '%" + searchTxt + "%') ";
-		}else {
-			sql2 = "and " + searchType + " like '%" + searchTxt + "%' ";
-		}
-		if(!searchTxt.equals("")) {
-			sql += sql2;
-		}
 		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				total = rs.getInt(1);
-			}
-		} catch (Exception e) {
+			total = (Integer) session.selectOne("selectTotal");
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(rs,pstmt,conn);
 		}
 		return total;
 	}
 	
 	public void updateHit(int brd_no) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql = "update j_recommendboard set brd_readcount=brd_readcount+1 where brd_no=?";
 		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, brd_no);
-			pstmt.executeUpdate();		
-		} catch (Exception e) {
+			session.update("updateHit", brd_no);
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(pstmt,conn);
 		}
 	}
 	
 	public List<J_RecommendBoard> selectList(int startRow, int endRow, String searchType, String searchTxt) {
 		List<J_RecommendBoard> list = new ArrayList<J_RecommendBoard>();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql2 ="";
-		if(searchType.equals("all")) {
-			sql2 = "and (brd_subject like '%" + searchTxt + "%' or brd_content like '%" + searchTxt + "%') ";
-		}else {
-			sql2 = "and " + searchType + " like '%" + searchTxt + "%' ";
-		}
-		if(searchTxt.equals("")){
-			sql2 = "";
-		}
-		String sql = "select * from (select rowNum rn, a.* from (select jrb.*, m_nick, c.c_value as rc_value, (select count(*) from j_recommend2 jr2 where jr2.brd_no = jrb.brd_no) recocount from j_recommendboard jrb, j_member m, j_code c where jrb.m_no=m.m_no and jrb.rc_code=c.c_minor and brd_del_yn='n' " + sql2 + "order by ref desc, re_step) a) where rn between ? and ?";
+		HashMap<String, Integer> hm = new HashMap<>();
+		hm.put("startRow", startRow);
+		hm.put("endRow", endRow);
 		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, startRow);
-			pstmt.setInt(2, endRow);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				J_RecommendBoard recommendboard = new J_RecommendBoard();
-				recommendboard.setBrd_no(rs.getInt("brd_no"));
-				recommendboard.setBrd_subject(rs.getString("brd_subject"));
-				recommendboard.setBrd_content(rs.getString("brd_content"));
-				recommendboard.setBrd_readcount(rs.getInt("brd_readcount"));
-				recommendboard.setBrd_ip(rs.getString("brd_ip"));
-				recommendboard.setBrd_reg_date(rs.getDate("brd_reg_date"));
-				recommendboard.setBrd_update_date(rs.getDate("brd_update_date"));
-				recommendboard.setBrd_dey_yn(rs.getString("brd_del_yn"));
-				recommendboard.setRef(rs.getInt("ref"));
-				recommendboard.setRe_step(rs.getInt("re_step"));
-				recommendboard.setRe_level(rs.getInt("re_level"));
-				recommendboard.setM_no(rs.getInt("m_no"));
-				recommendboard.setM_nick(rs.getString("m_nick"));
-				recommendboard.setRc_value(rs.getString("rc_value"));
-				recommendboard.setRecocount(rs.getInt("recocount"));
-				list.add(recommendboard);
-			}
-		} catch (Exception e) {
+			list = session.selectList("selectList", hm);
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(rs,pstmt,conn);
-		}		
+		}
 		return list;
 	}
 	
@@ -161,88 +100,38 @@ public class J_RecommendBoardDao {
 			pstmt.setInt(8, recommendboard.getM_no());
 			pstmt.setString(9, recommendboard.getRc_code());
 			result = pstmt.executeUpdate();
-		} catch (Exception e) {
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(rs,pstmt,conn);
 		}
 		return result;
 	}
 	
 	public J_RecommendBoard select(int brd_no) {
-		J_RecommendBoard recommendboard = new J_RecommendBoard();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = 
-				"select jrb.*, m.m_nick, c.c_value as rc_value, (select count(*) from j_recommend2 jr2 where jr2.brd_no = jrb.brd_no) recocount from j_recommendboard jrb, j_member m, j_code c where jrb.brd_no=? and jrb.m_no=m.m_no and jrb.rc_code=c.c_minor";
+		J_RecommendBoard jrb = null;
 		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, brd_no);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				recommendboard.setBrd_no(rs.getInt("brd_no"));
-				recommendboard.setBrd_subject(rs.getString("brd_subject"));
-				recommendboard.setBrd_content(rs.getString("brd_content"));
-				recommendboard.setBrd_readcount(rs.getInt("brd_readcount"));
-				recommendboard.setBrd_ip(rs.getString("brd_ip"));
-				recommendboard.setBrd_reg_date(rs.getDate("brd_reg_date"));
-				recommendboard.setBrd_update_date(rs.getDate("brd_update_date"));
-				recommendboard.setRef(rs.getInt("ref"));
-				recommendboard.setRe_step(rs.getInt("re_step"));
-				recommendboard.setRe_level(rs.getInt("re_level"));
-				recommendboard.setM_no(rs.getInt("m_no"));
-				recommendboard.setM_nick(rs.getString("m_nick"));
-				recommendboard.setRc_code(rs.getString("rc_code"));
-				recommendboard.setRc_value(rs.getString("rc_value"));
-				recommendboard.setRecocount(rs.getInt("recocount"));				
-			}
-		} catch (Exception e) {
+			jrb = (J_RecommendBoard) session.selectOne("select", brd_no);
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(rs,pstmt,conn);
 		}
-		return recommendboard;
+		return jrb;
 	}
 	
 	public int update(J_RecommendBoard recommendboard) {
 		int result = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql = "update j_recommendboard set brd_subject=?,brd_content=?,rc_code=?,brd_update_date=sysdate where brd_no=?";
 		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, recommendboard.getBrd_subject());
-			pstmt.setString(2, recommendboard.getBrd_content());
-			pstmt.setString(3, recommendboard.getRc_code());
-			pstmt.setInt(4, recommendboard.getBrd_no());
-			System.out.println(recommendboard);
-			result = pstmt.executeUpdate();
-		} catch (Exception e) {
-			System.out.println(recommendboard);
+			result = session.update("update", recommendboard);
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(pstmt, conn);
 		}
 		return result;
 	}
 
 	public int delete(int brd_no) {
 		int result = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql = "update j_recommendboard set brd_del_yn='y' where brd_no=?";
 		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, brd_no);
-			result = pstmt.executeUpdate();
-		} catch (Exception e) {
+			result = session.update("delete", brd_no);
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(pstmt, conn);
 		}
 		return result;
 	}
@@ -261,21 +150,15 @@ public class J_RecommendBoardDao {
 			if (rs.next()) {
 				recommendboard.setM_passwd(rs.getString("m_passwd"));
 			}
-		} catch (Exception e) {
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
-		} finally {
-			dbClose(rs, pstmt, conn);
 		}
 		return recommendboard;
 	}
 	
 	public int selectRecommend(String m_no, int brd_no) {
 		int result = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = "select * from j_recommend2 where m_no = ? and brd_no = ?";
-		try{
+		try {
 			conn = getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, m_no);
@@ -286,8 +169,6 @@ public class J_RecommendBoardDao {
 			}
 		}catch(Exception e){
 			System.out.println(e.getMessage());
-		}finally {
-			dbClose(rs, pstmt, conn);
 		}
 		return result;
 	}
@@ -331,41 +212,10 @@ public class J_RecommendBoardDao {
 						result = -1;
 					}
 				}
-			} catch (SQLException e) {
+			}catch (SQLException e) {
 				e.printStackTrace();
-			}finally {
-				dbClose(rs, pstmt, conn);
 			}
 		return result; 
 	}
 	
-	public void dbClose(PreparedStatement pstmt, Connection conn) {
-		try {
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}// dbClose(pstmt,conn)
-
-	public void dbClose(ResultSet rs, PreparedStatement pstmt, Connection conn) {
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} // dbClose(rs,pstmt,conn)
-	}
-
 }

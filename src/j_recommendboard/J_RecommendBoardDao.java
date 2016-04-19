b4670e72f98b15da6163736bdfc6a5941e16bd24
곽지은 <je928@daum.net>
2016-04-19 20:07:49 +0900

    추천게시판 마이바티스 완료!

--- src/j_recommendboard/J_RecommendBoardDao.java
+++ src/j_recommendboard/J_RecommendBoardDao.java
@@ -64,48 +64,24 @@ public class J_RecommendBoardDao {
 		return list;
 	}
 	
-	/*public int insert(J_RecommendBoard recommendboard) {
+	public int insert(J_RecommendBoard recommendboard) {
 		int result = 0, number = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = "insert into j_recommendboard values(?,?,?,0,?,sysdate,null,'n',?,?,?,?,?)";
-		String sql1 = "select nvl(max(brd_no),0)+1 from j_recommendboard";
-		String sql2 = "update j_recommendboard set re_step=re_step+1 where ref=? and re_step>?";
 		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql1);
-			rs = pstmt.executeQuery();
-			if (rs.next())
-				number = rs.getInt(1);
-			pstmt.close();
+			number = (int)session.selectOne("insertNum");
+			recommendboard.setBrd_no(number);
 			if (recommendboard.getBrd_no() != 0) {
-				pstmt = conn.prepareStatement(sql2);
-				pstmt.setInt(1, recommendboard.getRef());
-				pstmt.setInt(2, recommendboard.getRe_step());
-				pstmt.executeUpdate();
-				pstmt.close();
+				session.update("stepreco", recommendboard);
 				recommendboard.setRe_level(recommendboard.getRe_level() + 1);
 				recommendboard.setRe_step(recommendboard.getRe_step() + 1);
 			} else {
 				recommendboard.setRef(number);
 			}
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, number);
-			pstmt.setString(2, recommendboard.getBrd_subject());
-			pstmt.setString(3, recommendboard.getBrd_content());
-			pstmt.setString(4, recommendboard.getBrd_ip());
-			pstmt.setInt(5, recommendboard.getRef());
-			pstmt.setInt(6, recommendboard.getRe_step());
-			pstmt.setInt(7, recommendboard.getRe_level());
-			pstmt.setInt(8, recommendboard.getM_no());
-			pstmt.setString(9, recommendboard.getRc_code());
-			result = pstmt.executeUpdate();
+			result = session.insert("insertreco", recommendboard);
 		}catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		return result;
-	}*/
+	}
 	
 	public J_RecommendBoard select(int brd_no) {
 		J_RecommendBoard jrb = null;
